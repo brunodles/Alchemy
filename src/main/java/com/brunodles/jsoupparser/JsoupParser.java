f9a70427637e5873ce1562d38a0236551d0e9d75
Bruno de Lima e Silva <bruno.silva@concrete.com.br>
2018-07-11 14:12:43 -0300

    Refactor JsoupParser.ProxyHandler

--- src/main/java/com/brunodles/jsoupparser/JsoupParser.java
+++ src/main/java/com/brunodles/jsoupparser/JsoupParser.java
@@ -48,14 +48,11 @@ public class JsoupParser {
 
         @Override
         public Object invoke(Object o, Method method, Object[] objects) {
+            final String methodName = method.getName();
+            checkReturnType(method, methodName);
+
             if (resultCache.containsKey(method))
                 return resultCache.get(method);
-
-            final String methodName = method.getName();
-
-            if (method.getReturnType() == Void.TYPE)
-                throw new InvalidResultException(methodName);
-
             if ("hashcode".equalsIgnoreCase(methodName))
                 return this.hashCode();
             if ("toString".equalsIgnoreCase(methodName))
@@ -63,38 +60,20 @@ public class JsoupParser {
             if ("equals".equalsIgnoreCase(methodName))
                 return proxyEquals(this, objects[0]);
 
-            CssSelector cssSelector = method.getAnnotation(CssSelector.class);
-            if (cssSelector == null)
-                throw new MissingSelectorException(methodName);
+            CssSelector cssSelector = getAnnotation(method, methodName);
             final String selector = cssSelector.selector();
 
-            final Element element = document.selectFirst(selector);
-            if (element == null)
-                throw new InvalidSelectorException(methodName, selector);
+            final Element element = getElement(methodName, selector, document);
 
-            final ElementCollector<?> collector;
-            final Class<? extends ElementCollector> collectorClass = cssSelector.parser();
-            try {
-                collector = collectorClass.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new InvalidCollectorException(methodName, collectorClass, e);
-            }
+            final Object elementValue = getElementValue(method, methodName, element, cssSelector);
 
-            final Transformer transformer;
             final Class<? extends Transformer> transformerClass = cssSelector.transformer();
-            try {
-                transformer = transformerClass.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new InvalidTransformerException(methodName, transformerClass, e);
-            }
+            final Transformer transformer = getTransformer(methodName, transformerClass);
 
-            Object elementValue = null;
-            try {
-                elementValue = collector.collect(jsoupParser, element, method);
-            } catch (Exception e) {
-                throw new CollectorException(methodName, collectorClass, e);
-            }
+            return getResult(method, methodName, transformerClass, transformer, elementValue, resultCache);
+        }
 
+        private static Object getResult(Method method, String methodName, Class<? extends Transformer> transformerClass, Transformer transformer, Object elementValue, HashMap<Method, Object> resultCache) {
             final Object result;
             try {
                 result = transformer.transform(elementValue);
@@ -103,6 +82,60 @@ public class JsoupParser {
                 throw new TransformerException(methodName, transformerClass, e);
             }
             return result;
+        }
+
+        private Object getElementValue(Method method, String methodName, Element element, CssSelector cssSelector) {
+            final Class<? extends ElementCollector> collectorClass = cssSelector.parser();
+            final ElementCollector<?> collector = getElementCollector(methodName, collectorClass);
+
+            Object elementValue;
+            try {
+                elementValue = collector.collect(jsoupParser, element, method);
+            } catch (Exception e) {
+                throw new CollectorException(methodName, collectorClass, e);
+            }
+            return elementValue;
+        }
+
+        private Transformer getTransformer(String methodName, Class<? extends Transformer> transformerClass) {
+            final Transformer transformer;
+            try {
+                transformer = transformerClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new InvalidTransformerException(methodName, transformerClass, e);
+            }
+            return transformer;
+        }
+
+        private static ElementCollector<?> getElementCollector(String methodName, Class<? extends ElementCollector> collectorClass) {
+            final ElementCollector<?> collector;
+            try {
+                collector = collectorClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new InvalidCollectorException(methodName, collectorClass, e);
+            }
+            return collector;
+        }
+
+        @NotNull
+        private static Element getElement(String methodName, String selector, Element document) {
+            final Element element = document.selectFirst(selector);
+            if (element == null)
+                throw new InvalidSelectorException(methodName, selector);
+            return element;
+        }
+
+        @NotNull
+        private static CssSelector getAnnotation(Method method, String methodName) {
+            CssSelector cssSelector = method.getAnnotation(CssSelector.class);
+            if (cssSelector == null)
+                throw new MissingSelectorException(methodName);
+            return cssSelector;
+        }
+
+        private static void checkReturnType(Method method, String methodName) {
+            if (method.getReturnType() == Void.TYPE)
+                throw new InvalidResultException(methodName);
         }
 
         private Object proxyEquals(ProxyHandler proxyHandler, Object other) {
