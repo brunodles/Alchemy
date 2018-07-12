9b6ff043abc491cb12502c899ca6bf26411f18d4
Bruno de Lima e Silva <bruno.silva@concrete.com.br>
2018-07-12 14:49:52 -0300

    Update NestedCollector to decode nested collections

--- src/main/java/com/brunodles/jsoupparser/collectors/NestedCollector.java
+++ src/main/java/com/brunodles/jsoupparser/collectors/NestedCollector.java
@@ -7,12 +7,20 @@ import org.jetbrains.annotations.Nullable;
 import org.jsoup.nodes.Element;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
 
 public class NestedCollector implements ElementCollector<Object> {
 
     @Nullable
     @Override
     public Object collect(@NotNull JsoupParser jsoupParser, @NotNull Element value, @NotNull Method method) {
-        return jsoupParser.parseElement(value, method.getReturnType());
+        final Class<?> returnType = method.getReturnType();
+        if (Collection.class.isAssignableFrom(returnType)) {
+            ParameterizedType genericReturnType = (ParameterizedType) method.getGenericReturnType();
+            final Class<?> returnClass = (Class<?>) genericReturnType.getActualTypeArguments()[0];
+            return jsoupParser.parseElement(value, returnClass);
+        }
+        return jsoupParser.parseElement(value, returnType);
     }
 }
