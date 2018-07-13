9645b3ad026480596b41caf18c456258a82a3829
Bruno de Lima e Silva <bruno.silva@concrete.com.br>
2018-07-13 13:21:29 -0300

    Add tests to parseUrl method

---
+++ src/test/java/com/brunodles/jsoupparser/JsoupParserTest/WhenParseUrl.java
@@ -0,0 +1,59 @@
+package com.brunodles.jsoupparser.JsoupParserTest;
+
+import com.brunodles.jsoupparser.JsoupParser;
+import com.brunodles.jsoupparser.UriResolver;
+import com.brunodles.jsoupparser.doubles.SimpleModel;
+import com.brunodles.jsoupparser.doubles.UnknownException;
+import com.brunodles.jsoupparser.exceptions.ResolverException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@RunWith(JUnit4.class)
+public class WhenParseUrl {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Mock
+    private UriResolver uriResolver;
+    private JsoupParser jsoupParser;
+
+    @Before
+    public void setUp() {
+        jsoupParser = new JsoupParser(uriResolver);
+    }
+
+    @Test
+    public void shouldCallUriResolver() throws IOException {
+        when(uriResolver.htmlGet(anyString())).thenReturn("");
+
+        SimpleModel simpleModel = jsoupParser.parseUrl("res:whatIsThis", SimpleModel.class);
+
+        verify(uriResolver, only()).htmlGet(eq("res:whatIsThis"));
+    }
+
+    @Test
+    public void whenResolverThrowsException_shouldEncapsulateInResolverException() throws IOException {
+        when(uriResolver.htmlGet(anyString())).thenThrow(UnknownException.class);
+        expectedException.expect(ResolverException.class);
+        expectedException.expectCause(any(UnknownException.class));
+
+        jsoupParser.parseUrl("any", SimpleModel.class);
+    }
+}
