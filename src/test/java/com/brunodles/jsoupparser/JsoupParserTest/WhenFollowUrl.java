0a8c720a2b6595e66ae3de8c9598bca8222dd0c2
Bruno de Lima e Silva <bruno.silva@concrete.com.br>
2018-07-16 12:19:33 -0300

    Add tests to follow links

---
+++ src/test/java/com/brunodles/jsoupparser/JsoupParserTest/WhenFollowUrl.java
@@ -0,0 +1,36 @@
+package com.brunodles.jsoupparser.JsoupParserTest;
+
+import com.brunodles.jsoupparser.JsoupParser;
+import com.brunodles.jsoupparser.doubles.FollowModel;
+import com.brunodles.jsoupparser.doubles.SimpleModel;
+import com.brunodles.test.ResourceUriResolver;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(JUnit4.class)
+public class WhenFollowUrl {
+
+    private static JsoupParser parser;
+    private FollowModel follow;
+
+    @BeforeClass
+    public static void setupParser() {
+        parser = new JsoupParser(new ResourceUriResolver());
+    }
+
+    @Before
+    public void setupFollowModel() {
+        follow = parser.parseUrl("follow.html", FollowModel.class);
+    }
+
+    @Test
+    public void givenWhenThen() {
+        SimpleModel simple = follow.simple();
+        assertEquals("Jsoup Parser", simple.title());
+    }
+}
---
+++ src/test/java/com/brunodles/jsoupparser/doubles/FollowModel.java
@@ -0,0 +1,16 @@
+package com.brunodles.jsoupparser.doubles;
+
+import com.brunodles.jsoupparser.CssSelector;
+import com.brunodles.jsoupparser.collectors.AttrElementCollector;
+import com.brunodles.jsoupparser.collectors.AttrFollowUrlElementCollector;
+
+public interface FollowModel {
+
+    @CssSelector(selector = ".something a#simple", collector = AttrFollowUrlElementCollector.class)
+    @AttrElementCollector.Settings(attrName = "href")
+    SimpleModel simple();
+
+    @CssSelector(selector = ".something a#collections", collector = AttrFollowUrlElementCollector.class)
+    @AttrElementCollector.Settings(attrName = "href")
+    CollectionsModel collections();
+}
