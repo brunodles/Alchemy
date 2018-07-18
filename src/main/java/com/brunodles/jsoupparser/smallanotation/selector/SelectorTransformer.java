bdb6d4d34745380041f3b059a911cdae1cf99a67
Bruno de Lima <dlimaun@gmail.com>
2018-07-18 18:51:02 -0300

    Create a transformer for Selector

---
+++ src/main/java/com/brunodles/jsoupparser/smallanotation/selector/SelectorTransformer.java
@@ -0,0 +1,24 @@
+package com.brunodles.jsoupparser.smallanotation.selector;
+
+import com.brunodles.jsoupparser.Transformer;
+import com.brunodles.jsoupparser.exceptions.InvalidSelectorException;
+import com.brunodles.jsoupparser.smallanotation.AnnotationInvocation;
+import com.brunodles.jsoupparser.smallanotation.TransformerFor;
+import org.jsoup.select.Elements;
+
+@TransformerFor(Selector.class)
+public class SelectorTransformer implements Transformer<AnnotationInvocation<Selector, Elements>, Elements> {
+
+    @Override
+    public Elements transform(AnnotationInvocation<Selector, Elements> value) {
+        final String selector = value.annotation.value();
+        final Elements result;
+        if (value.result == null)
+            result = value.documentSelect(selector);
+        else
+            result = value.result.select(selector);
+        if (result == null || result.isEmpty())
+            throw new InvalidSelectorException(value.methodName, selector);
+        return result;
+    }
+}
