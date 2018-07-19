cbf97546cfbb668c98ba2f0f710edefaea33b86a
Bruno de Lima <dlimaun@gmail.com>
2018-07-19 17:42:08 -0300

    Rename FollowTransformer to Navigate

--- src/main/java/com/brunodles/jsoupparser/smallanotation/navigate/Navigate.java
+++ src/main/java/com/brunodles/jsoupparser/smallanotation/navigate/Navigate.java
@@ -1,9 +1,9 @@
-package com.brunodles.jsoupparser.smallanotation.annotations;
+package com.brunodles.jsoupparser.smallanotation.navigate;
 
 import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface FollowTransformer {
+public @interface Navigate {
 }
