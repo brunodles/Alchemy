4e36cb2b7460010e004a3a878e49d301e8164380
Bruno de Lima <dlimaun@gmail.com>
2018-07-19 17:01:07 -0300

    Update AnnotationInvocation to extends MethodInvocation

--- src/main/java/com/brunodles/jsoupparser/smallanotation/AnnotationInvocation.java
+++ src/main/java/com/brunodles/jsoupparser/smallanotation/AnnotationInvocation.java
@@ -1,25 +1,16 @@
 package com.brunodles.jsoupparser.smallanotation;
 
 import com.brunodles.jsoupparser.MethodInvocation;
-import com.brunodles.jsoupparser.ProxyHandler;
 import org.jsoup.select.Elements;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 
-public class AnnotationInvocation<ANNOTATION_TYPE extends Annotation, INPUT_TYPE> {
-    public final ProxyHandler proxyHandler;
-    public final Method method;
-    public final Object[] parameters;
-    public final String methodName;
+public class AnnotationInvocation<ANNOTATION_TYPE extends Annotation, INPUT_TYPE> extends MethodInvocation {
     public final ANNOTATION_TYPE annotation;
     public final INPUT_TYPE result;
 
     public AnnotationInvocation(MethodInvocation invocation, ANNOTATION_TYPE annotation, INPUT_TYPE result) {
-        proxyHandler = invocation.proxyHandler;
-        method = invocation.method;
-        parameters = invocation.parameters;
-        methodName = invocation.methodName;
+        super(invocation.proxyHandler, invocation.method, invocation.parameters);
         this.annotation = annotation;
         this.result = result;
     }
