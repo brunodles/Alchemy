8635f6a21e376fed4b2db1ee74738ee8115d8eed
Bruno de Lima <dlimaun@gmail.com>
2018-07-24 17:04:38 -0300

    Update NestedTransformer types
    This will make NestedTransformer use List<Element> instead of Elements

--- src/main/java/com/brunodles/jsoupparser/smallanotation/nested/NestedTransformer.java
+++ src/main/java/com/brunodles/jsoupparser/smallanotation/nested/NestedTransformer.java
@@ -4,19 +4,18 @@ import com.brunodles.jsoupparser.Transformer;
 import com.brunodles.jsoupparser.smallanotation.AnnotationInvocation;
 import com.brunodles.jsoupparser.smallanotation.TransformerFor;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @TransformerFor(Nested.class)
-public class NestedTransformer<OUTPUT> implements Transformer<AnnotationInvocation<Nested, Elements>, List<OUTPUT>> {
+public class NestedTransformer<OUTPUT> implements Transformer<AnnotationInvocation<Nested, List<Element>>, List<OUTPUT>> {
 
     @Override
-    public List<OUTPUT> transform(AnnotationInvocation<Nested, Elements> value) {
+    public List<OUTPUT> transform(AnnotationInvocation<Nested, List<Element>> value) {
         ArrayList<OUTPUT> result = new ArrayList<>();
         for (Element element : value.result) {
-            Object object =value.proxyHandler.jsoupParser.parseElement(element, value.getMethodRealReturnType());
+            Object object = value.proxyHandler.jsoupParser.parseElement(element, value.getMethodRealReturnType());
             result.add((OUTPUT) object);
         }
         return result;
