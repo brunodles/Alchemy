45f16e0195b93011a9fb67f273833ea98bd57e80
Bruno de Lima <dlimaun@gmail.com>
2018-07-24 16:49:46 -0300

    Update JsoupParser to return jsoup Element

--- src/main/java/com/brunodles/jsoupparser/JsoupParser.java
+++ src/main/java/com/brunodles/jsoupparser/JsoupParser.java
@@ -2,18 +2,12 @@ package com.brunodles.jsoupparser;
 
 import com.brunodles.jsoupparser.bigannotation.BigInvocationHandler;
 import com.brunodles.jsoupparser.exceptions.ResolverException;
-import com.brunodles.jsoupparser.smallanotation.selector.Selector;
-import com.brunodles.jsoupparser.smallanotation.selector.SelectorTransformer;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Proxy;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class JsoupParser {
@@ -53,6 +47,8 @@ public class JsoupParser {
 
     public <T> T parseHtml(@NotNull String html, @NotNull Class<T> interfaceClass) {
         Document document = Jsoup.parse(html);
+        if (Element.class.isAssignableFrom(interfaceClass))
+            return (T) document;
         return parseElement(document, interfaceClass);
     }
 
