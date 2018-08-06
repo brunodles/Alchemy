a6cbea0292235ffd79485f8da2af4707049338a4
Bruno de Lima <dlimaun@gmail.com>
2018-08-06 15:38:36 -0300

    Extract code to TransmutationRule

---
+++ Alchemist/src/test/java/com/brunodles/alchemist/TransmutationRule.java
@@ -0,0 +1,72 @@
+package com.brunodles.alchemist;
+
+import com.brunodles.alchemist.usevalueof.UseValueOf;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TransmutationRule<AnnotationT extends Annotation> implements TestRule {
+
+    public final AnnotationTransmutation<AnnotationT, Object, Object> transmutation;
+    @Mock
+    public ProxyHandler proxyHandler;
+    @Mock
+    public Method method;
+    @Mock
+    public AnnotationT annotation;
+
+    public TransmutationRule(AnnotationTransmutation<AnnotationT, Object, Object> transmutation) {
+        this.transmutation = transmutation;
+    }
+
+    @Override
+    public Statement apply(final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                MockitoAnnotations.initMocks(TransmutationRule.this);
+                base.evaluate();
+            }
+        };
+    }
+
+    public void whenMethodNameReturn(String methodName) {
+        when(method.getName()).thenReturn(methodName);
+    }
+
+    @SuppressWarnings("unchecked")
+    @SafeVarargs
+    public final <INPUT, OUTPUT> AnnotationInvocation<AnnotationT, List<INPUT>> buildInvocation(
+            final Class<OUTPUT> returnClass, final INPUT... inputs) {
+        when(method.getReturnType()).thenReturn((Class) List.class);
+        ParameterizedType type = mock(ParameterizedType.class);
+        when(type.getActualTypeArguments()).thenReturn(new Type[]{returnClass});
+        when(method.getGenericReturnType()).thenReturn(type);
+        return buildAnnotationInvocation(inputs);
+    }
+
+    @SuppressWarnings("unchecked")
+    @SafeVarargs
+    public final <T> AnnotationInvocation<AnnotationT, List<T>> buildAnnotationInvocation(final T... input) {
+        Object[] parameters = new Object[]{};
+        MethodInvocation methodInvocation = new MethodInvocation(proxyHandler, method, parameters);
+        return new AnnotationInvocation(methodInvocation, annotation, Arrays.asList(input));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <INPUT, OUTPUT> List<OUTPUT> transform(AnnotationInvocation<AnnotationT, INPUT> invocation) {
+        return (List<OUTPUT>) transmutation.transform((AnnotationInvocation<AnnotationT, Object>) invocation);
+    }
+}
--- Alchemist/src/test/java/com/brunodles/alchemist/usevalueof/UseValueOfTransmutationTest.java
+++ Alchemist/src/test/java/com/brunodles/alchemist/usevalueof/UseValueOfTransmutationTest.java
@@ -1,110 +1,70 @@
 package com.brunodles.alchemist.usevalueof;
 
 import com.brunodles.alchemist.AnnotationInvocation;
-import com.brunodles.alchemist.MethodInvocation;
-import com.brunodles.alchemist.ProxyHandler;
+import com.brunodles.alchemist.TransmutationRule;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class UseValueOfTransmutationTest {
 
-    private final UseValueOfTransmutation transformer = new UseValueOfTransmutation();
-
     @Rule
-    public MockitoRule mockito = MockitoJUnit.rule();
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
-    @Mock
-    ProxyHandler proxyHandler;
-    @Mock
-    Method method;
-    @Mock
-    UseValueOf annotation;
+    public TransmutationRule<UseValueOf> transmutationRule = new TransmutationRule(new UseValueOfTransmutation());
 
     @Before
     public void setup() {
-        when(method.getName()).thenReturn("methodName");
+        transmutationRule.whenMethodNameReturn("methodName");
     }
 
     @Test
     public void whenParseToInteger_shouldParse() {
-        AnnotationInvocation<UseValueOf, List<String>> invocation = buildInvocation(Integer.class, "123");
+        AnnotationInvocation<UseValueOf, List<String>> invocation = transmutationRule
+                .buildInvocation(Integer.class, "123");
 
-        List<Integer> result = transform(invocation);
+        List<Integer> result = transmutationRule.transform(invocation);
 
         assertEquals(new Integer(123), result.get(0));
     }
 
     @Test
     public void whenParseToLong_shouldParse() {
-        AnnotationInvocation<UseValueOf, List<String>> invocation = buildInvocation(Long.class,
+        AnnotationInvocation<UseValueOf, List<String>> invocation = transmutationRule.buildInvocation(Long.class,
                 "2132312132331232121");
 
-        List<Long> result = transform(invocation);
+        List<Long> result = transmutationRule.transform(invocation);
 
         assertEquals(new Long(2132312132331232121L), result.get(0));
     }
 
     @Test
     public void whenParseToFloat_shouldParse() {
-        AnnotationInvocation<UseValueOf, List<String>> invocation = buildInvocation(Float.class,
+        AnnotationInvocation<UseValueOf, List<String>> invocation = transmutationRule.buildInvocation(Float.class,
                 "212121321321.3221332113321123213213");
 
-        List<Float> result = transform(invocation);
+        List<Float> result = transmutationRule.transform(invocation);
 
         assertEquals(212121321321.3221332113321123213213F, result.get(0), 0.0000000000000000000001F);
     }
 
     @Test(expected = RuntimeException.class)
     public void whenClassDoesNotHaveValueOfMethod_shouldThrowException() {
-        AnnotationInvocation<UseValueOf, List<String>> invocation = buildInvocation(Character.TYPE, "1231323");
+        AnnotationInvocation<UseValueOf, List<String>> invocation = transmutationRule
+                .buildInvocation(Character.TYPE, "1231323");
 
-        List<Character> result = transform(invocation);
+        List<Character> result = transmutationRule.transform(invocation);
 
         fail();
     }
 
-    @SuppressWarnings("unchecked")
-    @SafeVarargs
-    private final <INPUT, OUTPUT> AnnotationInvocation<UseValueOf, List<INPUT>> buildInvocation(
-            final Class<OUTPUT> returnClass, final INPUT... inputs) {
-        when(method.getReturnType()).thenReturn((Class) List.class);
-        ParameterizedType type = mock(ParameterizedType.class);
-        when(type.getActualTypeArguments()).thenReturn(new Type[]{returnClass});
-        when(method.getGenericReturnType()).thenReturn(type);
-        return buildAnnotationInvocation(inputs);
-    }
-
-    @SuppressWarnings("unchecked")
-    private <INPUT, OUTPUT> List<OUTPUT> transform(AnnotationInvocation<UseValueOf, List<INPUT>> invocation) {
-        return (List<OUTPUT>) transformer.transform(invocation);
-    }
-
-    @SafeVarargs
-    private final <T> AnnotationInvocation<UseValueOf, List<T>> buildAnnotationInvocation(final T... input) {
-        Object[] parameters = new Object[]{};
-        MethodInvocation methodInvocation = new MethodInvocation(proxyHandler, method, parameters);
-        return new AnnotationInvocation<>(methodInvocation, annotation, Arrays.asList(input));
-    }
 }
