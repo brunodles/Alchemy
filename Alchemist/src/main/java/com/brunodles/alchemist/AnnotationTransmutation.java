122058cde798f7d84af935b32831bef92e1316d5
Bruno de Lima <dlimaun@gmail.com>
2018-08-06 18:57:30 -0300

    Fix generics name on AnnotationTransmutation

--- Alchemist/src/main/java/com/brunodles/alchemist/AnnotationTransmutation.java
+++ Alchemist/src/main/java/com/brunodles/alchemist/AnnotationTransmutation.java
@@ -2,6 +2,6 @@ package com.brunodles.alchemist;
 
 import java.lang.annotation.Annotation;
 
-public interface AnnotationTransmutation<ANNOTATION extends Annotation, INPUT, OUTPUT>
-        extends Transmutation<AnnotationInvocation<ANNOTATION, INPUT>, OUTPUT> {
+public interface AnnotationTransmutation<AnnotationT extends Annotation, INPUT, OUTPUT>
+        extends Transmutation<AnnotationInvocation<AnnotationT, INPUT>, OUTPUT> {
 }
